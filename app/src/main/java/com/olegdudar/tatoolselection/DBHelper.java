99c59fe3ea29390abe84519ec63b621458e134e9
Oleg Dudar <oleg_dudar@epam.com>
2016-06-30 18:37:04 +0300

    added db creation and recreation

--- app/src/main/java/com/olegdudar/tatoolselection/DBHelper.java
+++ app/src/main/java/com/olegdudar/tatoolselection/DBHelper.java
@@ -4,14 +4,23 @@ import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.util.Log;
 
-/**
- * Created by Oleg_Dudar on 6/30/2016.
- */
-public class DBHelper extends SQLiteOpenHelper {
+public class DBHelper extends SQLiteOpenHelper implements BaseColumns {
 
     private static final String DATABASE_NAME = "tatools.db";
     private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_TABLE = "tools";
+    public static final String TOOL_NAME_COLUMN = "tool_name";
+    public static final String VENDOR_COLUMN = "vendor";
+    public static final String PRICE_COLUMN = "price";
+
+    private static final String DATABASE_CREATE_SCRIPT = "create table "
+            + DATABASE_TABLE + " (" + BaseColumns._ID
+            + " integer primary key autoincrement, " + TOOL_NAME_COLUMN
+            + " text not null, " + VENDOR_COLUMN + " text, " + PRICE_COLUMN
+            + " double);";
 
     DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -29,11 +38,17 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(DATABASE_CREATE_SCRIPT);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // write to journal
+        Log.w("SQLite", "Updating from version " + oldVersion + " to version " + newVersion);
 
+        // remove old table and create new one
+        db.execSQL("DROP TABLE IF IT EXISTS " + DATABASE_TABLE);
+        // create new table
+        onCreate(db);
     }
 }
--- app/src/main/java/com/olegdudar/tatoolselection/MainActivity.java
+++ app/src/main/java/com/olegdudar/tatoolselection/MainActivity.java
@@ -1,13 +1,23 @@
 package com.olegdudar.tatoolselection;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DBHelper mDBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void recreateDB(View view){
+        mDBHelper = new DBHelper(this, "mydatabase.db", null, 1);
+        SQLiteDatabase sdb;
+        sdb = mDBHelper.getReadableDatabase();
     }
 }
