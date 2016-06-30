3bc0a104aef30f1e67dc3563cfc142647731a458
Oleg Dudar <oleg_dudar@epam.com>
2016-06-30 18:12:48 +0300

    Initial commit

---
+++ app/src/main/java/com/olegdudar/tatoolselection/AppTypeSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.olegdudar.tatoolselection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppTypeSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_type_selection);
+    }
+}
---
+++ app/src/main/java/com/olegdudar/tatoolselection/DBHelper.java
@@ -0,0 +1,39 @@
+package com.olegdudar.tatoolselection;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Oleg_Dudar on 6/30/2016.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "tatools.db";
+    private static final int DATABASE_VERSION = 1;
+
+    DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                          int version) {
+        super(context, name, factory, version);
+    }
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                          int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
